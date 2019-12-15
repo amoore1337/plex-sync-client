6df1371d08c045f66ca54b1ea2882b03c878e716
Austin Moore <amoore1337@gmail.com>
2019-12-15 12:52:34 -0500

    More wip

--- app/workers/download-content.js
+++ app/workers/download-content.js
@@ -13,23 +13,22 @@ const MANAGER_DOMAIN = 'https://192.168.1.205:1338';
   const filePath = path.resolve(__dirname, UNPACK_DIR, 'tmp.tar');
   const writer = fs.createWriteStream(filePath);
 
-  try {
-    const response = await axios({
-      url,
-      method: 'GET',
-      responseType: 'stream',
-    });
-
-    parentPort.postMessage("Howdy!!");
+  const promise = axios({
+    url,
+    method: 'GET',
+    responseType: 'stream',
+  }).then(response => {
+    parentPort.postMessage("Testing 123");
     response.data.pipe(writer);
 
     return new Promise((resolve, reject) => {
       writer.on('finish', resolve);
       writer.on('error', reject);
     });
-  } catch (err) {
-    console.error(err.toJSON());
-  }
+  }).catch(err => console.error(err));
+
+  parentPort.postMessage("Howdy!!");
   // Use manager-comm.service to start file download
   // Grab logic from test script and post messages back to parent with download status updates
+  return promise;
 })();
