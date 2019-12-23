d3bee1c47c70a8f10aa0f35f6218dea4894652f9
Austin Moore <amoore1337@gmail.com>
2019-12-23 15:28:03 -0500

    test

--- app/app.js
+++ app/app.js
@@ -39,6 +39,7 @@ module.exports = async (callback) => {
   // Run any pending migrations when the app starts up:
   try {
     const db = await dbConnection();
+    console.log(await db.run('PRAGMA journal_mode'))
     await runMigrations(db);
     await dbClose(db);
   } catch (error) {
