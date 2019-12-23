903e187a5b3678e8fa07e3972c0ccb9ee873fbc2
Austin Moore <amoore1337@gmail.com>
2019-12-23 15:23:02 -0500

    move to separate db connections

--- app/services/content-state-manager.service.js
+++ app/services/content-state-manager.service.js
@@ -45,15 +45,14 @@ exports.updateContentStatus = async function(token, type, status) {
 exports.completeContent = async function(token, type) {
   console.log('completing!');
   try {
-    const db = await dbConnection();
-
     if (type === 'movie') {
       console.log('marking movie as completed');
-      await markMovieAsCompleted(db, token);
+      await markMovieAsCompleted(token);
     } else if (type === 'season') {
-      await markSeasonAsCompleted(db, token);
+      await markSeasonAsCompleted(token);
     }
 
+    const db = await dbConnection();
     console.log('deleting pending record');
     await db.run(`DELETE FROM pending_download_requests WHERE token = "${token}";`)
     console.log('finished');
@@ -104,94 +103,110 @@ async function updateContent(token, type, status) {
   await dbClose(db);
 }
 
-async function markMovieAsCompleted(db, token) {
-  const dirMap = await getExistingMoviesMap();
-  const fsMovie = find(dirMap, { token });
+async function markMovieAsCompleted(token) {
+  let db;
+  try {
+    db = await dbConnection();
+    const dirMap = await getExistingMoviesMap();
+    const fsMovie = find(dirMap, { token });
 
-  console.log('adding local movie')
-  await db.run(insertQuery('local_movies', {
-    name: fsMovie.name,
-    token: token,
-    size: fsMovie.size,
-    created_at: Date.now(),
-  }));
-
-  console.log('updating remote movie');
-  await db.run(updateQuery('remote_movies', { status: 'completed' }) + ` WHERE token = "${token}"`);
-}
-
-async function markSeasonAsCompleted(db, token) {
-  const dirMap = await getExistingTvShowsMap();
-
-  const remoteShow = await db.get(`
-    SELECT shows.* FROM remote_tv_show_seasons seasons
-    LEFT JOIN remote_tv_shows shows ON shows.ROWID = seasons.remote_tv_show_id
-    WHERE seasons.token = "${token}"
-  `);
-
-  const fsShow = find(dirMap, { token: remoteShow.token });
-  const fsSeason = find(fsShow.children, { token });
-
-  // Find or create the show record:
-  await findOrCreate(db, 'local_tv_shows', {
-    name: fsShow.name,
-    token: remoteShow.token,
-    size: fsShow.size,
-  });
-  const localShow = await db.get(`SELECT *, ROWID FROM local_tv_shows WHERE token = "${remoteShow.token}"`);
-
-  await db.run(insertQuery('local_tv_show_seasons', {
-    name: fsSeason.name,
-    token: token,
-    size: fsSeason.size,
-    local_tv_show_id: localShow.rowid,
-    created_at: Date.now(),
-  }));
-  const localSeason = await db.get(`SELECT *, ROWID FROM local_tv_show_seasons WHERE token = "${token}"`);
-
-  const remoteEpisodes = await db.all(`
-    SELECT episodes.* FROM remote_tv_show_seasons seasons
-    LEFT JOIN remote_tv_show_episodes episodes ON episodes.remote_tv_show_season_id = seasons.ROWID
-    WHERE seasons.token = "${token}"
-  `);
-
-  let localEpisodeCount = 0;
-  for (let i = 0; i < remoteEpisodes.length; i++) {
-    const episode = remoteEpisodes[i];
-    const fsEpisode = find(fsSeason.children, { name: episode.name })
-    if (!fsEpisode) {
-      continue;
-    }
-    await db.run(insertQuery('local_tv_show_episodes', {
-      name: episode.name,
-      token: episode.token,
-      size: fsEpisode.size,
-      local_tv_show_season_id: localSeason.rowid,
+    console.log('adding local movie')
+    await db.run(insertQuery('local_movies', {
+      name: fsMovie.name,
+      token: token,
+      size: fsMovie.size,
       created_at: Date.now(),
     }));
-    localEpisodeCount++;
 
-    await db.run(updateQuery('remote_tv_show_episodes', { status: 'completed' }) + ` WHERE token = "${episode.token}"`);
+    console.log('updating remote movie');
+    await db.run(updateQuery('remote_movies', { status: 'completed' }) + ` WHERE token = "${token}"`);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClose(db);
   }
+}
 
-  await db.run(updateQuery('remote_tv_show_seasons', {
-    status: remoteEpisodes.length === localEpisodeCount ? 'completed' : 'incomplete'
-  }) + ` WHERE token = "${token}"`);
+async function markSeasonAsCompleted(token) {
+  let db;
+  try {
+    db = await dbConnection();
+    const dirMap = await getExistingTvShowsMap();
 
-  const totalLocalEpisodes = await db.get(`
-    SELECT COUNT(episodes.token) AS episode_count FROM local_tv_shows shows
-    LEFT JOIN local_tv_show_seasons seasons ON seasons.local_tv_show_id = shows.ROWID
-    LEFT JOIN local_tv_show_episodes episodes ON episodes.local_tv_show_season_id = seasons.ROWID
-    WHERE shows.token = "${remoteShow.token}"
-  `);
+    const remoteShow = await db.get(`
+      SELECT shows.* FROM remote_tv_show_seasons seasons
+      LEFT JOIN remote_tv_shows shows ON shows.ROWID = seasons.remote_tv_show_id
+      WHERE seasons.token = "${token}"
+    `);
 
-  const totalRemoteEpisodes = await db.get(`
-    SELECT COUNT(episodes.token) AS episode_count FROM remote_tv_shows shows
-    LEFT JOIN remote_tv_show_seasons seasons ON seasons.remote_tv_show_id = shows.ROWID
-    LEFT JOIN remote_tv_show_episodes episodes ON episodes.remote_tv_show_season_id = seasons.ROWID
-    WHERE shows.token = "${remoteShow.token}"
-  `);
+    const fsShow = find(dirMap, { token: remoteShow.token });
+    const fsSeason = find(fsShow.children, { token });
 
-  const newStatus = totalLocalEpisodes.episode_count === totalRemoteEpisodes.episode_count ? 'completed' : 'incomplete';
-  await db.run(updateQuery('remote_tv_shows', { status: newStatus }) + ` WHERE token = "${remoteShow.token}"`);
+    // Find or create the show record:
+    await findOrCreate(db, 'local_tv_shows', {
+      name: fsShow.name,
+      token: remoteShow.token,
+      size: fsShow.size,
+    });
+    const localShow = await db.get(`SELECT *, ROWID FROM local_tv_shows WHERE token = "${remoteShow.token}"`);
+
+    await db.run(insertQuery('local_tv_show_seasons', {
+      name: fsSeason.name,
+      token: token,
+      size: fsSeason.size,
+      local_tv_show_id: localShow.rowid,
+      created_at: Date.now(),
+    }));
+    const localSeason = await db.get(`SELECT *, ROWID FROM local_tv_show_seasons WHERE token = "${token}"`);
+
+    const remoteEpisodes = await db.all(`
+      SELECT episodes.* FROM remote_tv_show_seasons seasons
+      LEFT JOIN remote_tv_show_episodes episodes ON episodes.remote_tv_show_season_id = seasons.ROWID
+      WHERE seasons.token = "${token}"
+    `);
+
+    let localEpisodeCount = 0;
+    for (let i = 0; i < remoteEpisodes.length; i++) {
+      const episode = remoteEpisodes[i];
+      const fsEpisode = find(fsSeason.children, { name: episode.name })
+      if (!fsEpisode) {
+        continue;
+      }
+      await db.run(insertQuery('local_tv_show_episodes', {
+        name: episode.name,
+        token: episode.token,
+        size: fsEpisode.size,
+        local_tv_show_season_id: localSeason.rowid,
+        created_at: Date.now(),
+      }));
+      localEpisodeCount++;
+
+      await db.run(updateQuery('remote_tv_show_episodes', { status: 'completed' }) + ` WHERE token = "${episode.token}"`);
+    }
+
+    await db.run(updateQuery('remote_tv_show_seasons', {
+      status: remoteEpisodes.length === localEpisodeCount ? 'completed' : 'incomplete'
+    }) + ` WHERE token = "${token}"`);
+
+    const totalLocalEpisodes = await db.get(`
+      SELECT COUNT(episodes.token) AS episode_count FROM local_tv_shows shows
+      LEFT JOIN local_tv_show_seasons seasons ON seasons.local_tv_show_id = shows.ROWID
+      LEFT JOIN local_tv_show_episodes episodes ON episodes.local_tv_show_season_id = seasons.ROWID
+      WHERE shows.token = "${remoteShow.token}"
+    `);
+
+    const totalRemoteEpisodes = await db.get(`
+      SELECT COUNT(episodes.token) AS episode_count FROM remote_tv_shows shows
+      LEFT JOIN remote_tv_show_seasons seasons ON seasons.remote_tv_show_id = shows.ROWID
+      LEFT JOIN remote_tv_show_episodes episodes ON episodes.remote_tv_show_season_id = seasons.ROWID
+      WHERE shows.token = "${remoteShow.token}"
+    `);
+
+    const newStatus = totalLocalEpisodes.episode_count === totalRemoteEpisodes.episode_count ? 'completed' : 'incomplete';
+    await db.run(updateQuery('remote_tv_shows', { status: newStatus }) + ` WHERE token = "${remoteShow.token}"`);
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await dbClose(db);
+  }
 }
